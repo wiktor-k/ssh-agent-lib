58024c84fefe5177155c8b973db170cb8d98c733
Arthur Gautier <arthur.gautier@arista.com>
2024-02-13 22:54:04 -0800

    Fixup extension serialization
    This fixes #8, which turned out to be incorrect.
    
    The `ExtensionContent` object isn't really standard and pretty
    unexpected.
    
    This also includes a sample implementation of a `struct SessionBind` that
    will serialize and deserialize correctly.
    
    Signed-off-by: Arthur Gautier <arthur.gautier@arista.com>

--- src/proto/mod.rs
+++ src/proto/mod.rs
@@ -4,6 +4,7 @@ pub mod ser;
 #[macro_use]
 pub mod key_type;
 pub mod error;
+pub mod extension;
 pub mod message;
 pub mod private_key;
 pub mod public_key;
@@ -38,5 +39,51 @@ impl<'a, T: Serialize + Deserialize<'a>> Blob for T {
 
     fn from_blob(blob: &[u8]) -> ProtoResult<T> {
         from_bytes(blob)
+    }
+}
+
+pub mod recursive {
+    use super::{from_bytes, to_bytes};
+    use serde::{
+        de::{self, Deserializer, Visitor},
+        ser::{Error, Serializer},
+        Deserialize, Serialize,
+    };
+    use std::{fmt, marker::PhantomData};
+
+    pub fn serialize<T, S>(obj: &T, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: Serialize,
+        S: Serializer,
+    {
+        serializer.serialize_bytes(&to_bytes(obj).map_err(S::Error::custom)?)
+    }
+
+    pub fn deserialize<'de, T, D>(deserialize: D) -> Result<T, D::Error>
+    where
+        T: Deserialize<'de>,
+        D: Deserializer<'de>,
+    {
+        struct RecursiveVisitor<T>(PhantomData<T>);
+
+        impl<'de, T> Visitor<'de> for RecursiveVisitor<T>
+        where
+            T: Deserialize<'de>,
+        {
+            type Value = T;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                formatter.write_str("an integer between -2^31 and 2^31")
+            }
+
+            fn visit_bytes<E>(self, value: &[u8]) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                from_bytes(value).map_err(E::custom)
+            }
+        }
+
+        deserialize.deserialize_bytes(RecursiveVisitor(PhantomData::<T>))
     }
 }
