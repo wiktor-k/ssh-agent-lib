01697902fbae2b338687d458ab726e042bcbfc5e
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-03-26 17:48:59 +0100

    Add tests for more commands
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- tests/sign-and-verify.sh
+++ tests/sign-and-verify.sh
@@ -18,5 +18,13 @@ ssh-add -L | tee agent.pub
 ssh-keygen -Y sign -f agent.pub -n file < Cargo.toml > Cargo.toml.sig
 ssh-keygen -Y check-novalidate -n file -f agent.pub -s Cargo.toml.sig < Cargo.toml
 
-rm -rf ssh-agent.sock Cargo.toml.sig id_rsa id_rsa.pub agent.pub
+rm -rf Cargo.toml.sig id_rsa id_rsa.pub agent.pub
 
+# Test other commands:
+export SSH_ASKPASS=`pwd`/tests/pwd-test.sh
+# AddSmartcardKey
+echo | ssh-add -s test
+# Lock
+echo | ssh-add -x
+# Unlock
+echo | ssh-add -X
