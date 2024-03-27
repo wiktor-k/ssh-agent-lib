aa9f43440c48ca582eabd1f0bd1b017fb77a14a9
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-03-27 08:54:51 +0100

    Remove unused files
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- src/proto.rs
+++ src/proto.rs
@@ -1,12 +1,9 @@
-#[macro_use]
-pub mod key_type;
 pub mod error;
 pub mod extension;
 pub mod message;
 pub mod signature;
 
 pub use self::error::*;
-pub use self::key_type::*;
 pub use self::message::*;
 pub use self::signature::*;
 
