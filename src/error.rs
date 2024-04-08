e5e06e4e3765ae13f8dcd558a8c715b8fef2a4b1
Arthur Gautier <arthur.gautier@arista.com>
2024-04-08 09:43:58 -0700

    error: cleanup after error refactor
    Signed-off-by: Arthur Gautier <arthur.gautier@arista.com>

--- src/error.rs
+++ src/error.rs
@@ -4,7 +4,6 @@ use crate::proto::ProtoError;
 
 #[derive(Debug)]
 pub enum AgentError {
-    //Ssh(ssh_key::Error),
     Proto(ProtoError),
     IO(io::Error),
 }
@@ -15,12 +14,6 @@ impl From<ProtoError> for AgentError {
     }
 }
 
-//impl From<ssh_key::Error> for AgentError {
-//    fn from(e: ssh_key::Error) -> AgentError {
-//        AgentError::Ssh(e)
-//    }
-//}
-
 impl From<io::Error> for AgentError {
     fn from(e: io::Error) -> AgentError {
         AgentError::IO(e)
@@ -30,7 +23,6 @@ impl From<io::Error> for AgentError {
 impl std::fmt::Display for AgentError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            //AgentError::Ssh(e) => write!(f, "Agent: Ssh key error: {e}"),
             AgentError::Proto(proto) => write!(f, "Agent: Protocol error: {}", proto),
             AgentError::IO(error) => write!(f, "Agent: I/O error: {}", error),
         }
