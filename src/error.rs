6ad7111b892835eaf8756fbea68ea4616bfa62ee
Arthur Gautier <arthur.gautier@arista.com>
2024-04-08 12:21:38 -0700

    switch error handling to `thiserror`
    Signed-off-by: Arthur Gautier <arthur.gautier@arista.com>

--- src/error.rs
+++ src/error.rs
@@ -1,32 +1,13 @@
 use std::io;
 
+use thiserror::Error;
+
 use crate::proto::ProtoError;
 
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum AgentError {
-    Proto(ProtoError),
-    IO(io::Error),
+    #[error("Agent: Protocol error: {0}")]
+    Proto(#[from] ProtoError),
+    #[error("Agent: I/O error: {0}")]
+    IO(#[from] io::Error),
 }
-
-impl From<ProtoError> for AgentError {
-    fn from(e: ProtoError) -> AgentError {
-        AgentError::Proto(e)
-    }
-}
-
-impl From<io::Error> for AgentError {
-    fn from(e: io::Error) -> AgentError {
-        AgentError::IO(e)
-    }
-}
-
-impl std::fmt::Display for AgentError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            AgentError::Proto(proto) => write!(f, "Agent: Protocol error: {}", proto),
-            AgentError::IO(error) => write!(f, "Agent: I/O error: {}", error),
-        }
-    }
-}
-
-impl std::error::Error for AgentError {}
