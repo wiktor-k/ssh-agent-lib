83f35ca30ea1ac6ce2fe1bab5ef9bac4902bb4b8
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-04-16 09:16:45 +0200

    Print message being deserialized for `--nocapture` tests
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -5,13 +5,14 @@ use ssh_agent_lib::proto::{Request, Response};
 use ssh_encoding::{Decode, Encode};
 use testresult::TestResult;
 
-fn roundtrip<T: Decode + Encode>(path: PathBuf) -> TestResult
+fn roundtrip<T: Decode + Encode + std::fmt::Debug>(path: PathBuf) -> TestResult
 where
     T::Error: std::fmt::Display,
 {
     let serialized = std::fs::read(path)?;
     let mut bytes: &[u8] = &serialized;
     let message = T::decode(&mut bytes)?;
+    eprintln!("Message: {message:#?}");
     let mut out = vec![];
     message.encode(&mut out)?;
     assert_eq!(serialized, out);
