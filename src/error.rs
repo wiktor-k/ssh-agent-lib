3bcf37a20d4cdab070b88c14c2563d95cfef3681
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-04-18 13:57:25 +0200

    Migrate back to using `AgentError` in `Session` methods
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- src/error.rs
+++ src/error.rs
@@ -10,4 +10,12 @@ pub enum AgentError {
     Proto(#[from] ProtoError),
     #[error("Agent: I/O error: {0}")]
     IO(#[from] io::Error),
+    #[error("Other error: {0:#}")]
+    Other(#[from] Box<dyn std::error::Error + Send + Sync + 'static>),
+}
+
+impl AgentError {
+    pub fn other(error: impl std::error::Error + Send + Sync + 'static) -> Self {
+        Self::Other(Box::new(error))
+    }
 }
