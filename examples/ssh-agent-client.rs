df138edc9dbb04888392562e54164c98d507fd49
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-05-15 12:58:15 +0200

    Add an example of using SSH agent extensions
    Adds the `decrypt-derive-ids@metacode.biz` and `decrypt-derive@metacode.biz`
    extensions with encoding and decoding rules.
    
    The extension is used to facilitate curve 25519 decryption over SSH
    agent connections.
    
    This PR additionally makes the OpenPGP Card agent example implement
    these two extensions thus providing clients with public keys of its
    decryption keys. Additionally a `decrypt derive` extension similar to
    the sign request is implemented.
    
    The PGP wrapper example has been extended to emit encryption subkeys
    if the agent supports them. An extra `decrypt` subcommand has been
    added. The docs have been updated to showcase how to use the feature:
    
    ```sh
    echo I like strawberries | gpg -er 4EB27E153DDC454364B36B59A142E92C91BE3AD5 > /tmp/encrypted.pgp
    SSH_AUTH_SOCK=/tmp/ext-agent.sock cargo run --example pgp-wrapper -- decrypt < /tmp/encrypted.pgp
    ...
    I like strawberries
    ```
    
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- examples/ssh-agent-client.rs
+++ examples/ssh-agent-client.rs
@@ -1,6 +1,7 @@
 use service_binding::Binding;
-use ssh_agent_lib::client::connect;
-
+use ssh_agent_lib::{client::connect, proto::Extension};
+mod extensions;
+use extensions::{DecryptIdentities, RequestDecryptIdentities};
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(unix)]
@@ -15,6 +16,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         "Identities that this agent knows of: {:#?}",
         client.request_identities().await?
     );
+
+    if let Ok(Some(identities)) = client
+        .extension(Extension::new_message(RequestDecryptIdentities)?)
+        .await
+    {
+        let identities = identities.parse_message::<DecryptIdentities>()?;
+        eprintln!("Decrypt identities that this agent knows of: {identities:#?}",);
+    } else {
+        eprintln!("No decryption identities found.");
+    }
 
     Ok(())
 }
