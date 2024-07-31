995c4444cb70875c18ff80fdc9be0b214c291ee0
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-07-31 12:14:20 +0200

    WIP: Upgrade versions of RustCrypto to pre
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- fuzz/fuzz_targets/request_decode.rs
+++
@@ -1,9 +0,0 @@
-#![no_main]
-
-use libfuzzer_sys::fuzz_target;
-use ssh_agent_lib::proto::message::Request;
-use ssh_encoding::Decode;
-
-fuzz_target!(|data: &[u8]| {
-    let _ = Request::decode(&mut &data[..]);
-});
