14feb79fd9a846ca56fe2922322636c58d2efd44
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2026-09-09 09:53:10 +0200

    Update dependencies
    Signed-off-by: Wiktor Kwapisiewicz <wiktor@metacode.biz>

--- fuzz/fuzz_targets/request_decode.rs
+++ fuzz/fuzz_targets/request_decode.rs
@@ -1,8 +1,7 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use ssh_agent_lib::proto::message::Request;
-use ssh_encoding::Decode;
+use ssh_agent_lib::{proto::message::Request, ssh_encoding::Decode as _};
 
 fuzz_target!(|data: &[u8]| {
     let _ = Request::decode(&mut &data[..]);
